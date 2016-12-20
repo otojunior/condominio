82ce3c2f1bd4b39ebaa517e9a731f350d1cf9f3f
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2016-12-20 16:51:24 -0200

    Config. para TravisCI, Coveralls e Codacy

---
+++ src/test/java/org/otojunior/condominio/service/PropriedadesServiceTest.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package org.otojunior.condominio.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author 01456231650
+ *
+ */
+public class PropriedadesServiceTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link org.otojunior.condominio.service.PropriedadesService#PropriedadesService(java.lang.String)}.
+	 */
+	@Test
+	public final void testPropriedadesService() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.otojunior.condominio.service.PropriedadesService#obterParametros()}.
+	 */
+	@Test
+	public final void testObterParametros() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.otojunior.condominio.service.PropriedadesService#obterDados()}.
+	 */
+	@Test
+	public final void testObterDados() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ src/test/java/org/otojunior/condominio/service/RelatorioServiceTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package org.otojunior.condominio.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author 01456231650
+ *
+ */
+public class RelatorioServiceTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link org.otojunior.condominio.service.RelatorioService#compilarRelatorio(java.lang.String, java.util.Map, java.util.Collection)}.
+	 */
+	@Test
+	public final void testCompilarRelatorio() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.otojunior.condominio.service.RelatorioService#gerarRelatorio(java.lang.String, java.util.Map, java.util.Collection)}.
+	 */
+	@Test
+	public final void testGerarRelatorio() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
